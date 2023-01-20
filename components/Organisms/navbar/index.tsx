62db7bfbec359f9b4743f22f3514cf8da99860bf
A-fauzi <akhmadfauzi0101@gmail.com>
2023-01-20 19:39:51 +0700

    refactor login navbar component

--- components/Organisms/navbar/index.tsx
+++ components/Organisms/navbar/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Menu from "./Menu";
+import Auth from "./Auth";
+import ToggleMenu from "./ToggleMenu";
 
 export default function Navbar() {
   return (
@@ -7,24 +9,9 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
         <div className="container-fluid">
           <a className="navbar-br/and" href="#">
-            <Image
-              src="assets/icon/logo.svg"
-              alt={"logo"}
-              width={60}
-              height={60}
-            />
+            <Image src="assets/icon/logo.svg" alt={"logo"} width={60} height={60} />
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <ToggleMenu />
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
               <Menu title={"Home"} active url="/" />
@@ -32,6 +19,7 @@ export default function Navbar() {
               <Menu title={"Reward"} url="/reward" />
               <Menu title={"Discover"} url="/discover" />
               <Menu title={"Global Rank"} url="/global-rank" />
+              <Auth isLogin />
             </ul>
           </div>
         </div>
