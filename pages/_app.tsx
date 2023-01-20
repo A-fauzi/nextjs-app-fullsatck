42fdff240b8d73a191ea1d4971881035666af24c
A-fauzi <akhmadfauzi0101@gmail.com>
2023-01-20 16:44:40 +0700

    add transactions Detail  page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import "../styles/navbar-log-in.css";
 import "../styles/overview.css";
 import "../styles/sidebar.css";
 import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
 
 import { AppProps } from "next/app";
 import Head from "next/head";
