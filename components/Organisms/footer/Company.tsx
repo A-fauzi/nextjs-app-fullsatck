984ae0ba396b53a22eba5e8af5b149d4d2808764
A-fauzi <akhmadfauzi0101@gmail.com>
2023-01-20 23:56:14 +0700

    UP Landing Page | Refactor footer  component

---
+++ components/Organisms/footer/Company.tsx
@@ -0,0 +1,48 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+
+};
+export const Company = (props: Props) => {
+    return (
+        <div className="col-md-4 col-6 mb-lg-0 mb-25">
+            <p className="text-lg fw-semibold color-palette-1 mb-12">
+                Company
+            </p>
+            <ul className="list-unstyled">
+                <li className="mb-6">
+                    <a
+                        href=""
+                        className="text-lg color-palette-1 text-decoration-none"
+                    >
+                        About Us
+                    </a>
+                </li>
+                <li className="mb-6">
+                    <a
+                        href=""
+                        className="text-lg color-palette-1 text-decoration-none"
+                    >
+                        Press Release
+                    </a>
+                </li>
+                <li className="mb-6">
+                    <a
+                        href=""
+                        className="text-lg color-palette-1 text-decoration-none"
+                    >
+                        Terms of Use
+                    </a>
+                </li>
+                <li className="mb-6">
+                    <a
+                        href=""
+                        className="text-lg color-palette-1 text-decoration-none"
+                    >
+                        Privacy & Policy
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
